fa2ca1ad536ec10ee99b2aa6e98e32765a05a1ae
NoahSchiro <noahschiro@rocketmail.com>
2023-09-20 09:56:53 -0400

    Added more functions, testing, and typing

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from minigrad.autograd import Scalar
 
+def testing_operations():
 
-a = Scalar(4)
-b = Scalar(4.5)
+    a = Scalar(4)
+    b = Scalar(4.5)
+    print("Original values:")
+    print(a)
+    print(b)
 
-add = a+b
-sub = a-b
-mul = a*b
-div = a/b
+    add = a+b
+    sub = a-b
+    mul = a*b
+    div = a/b
+    pow = a**3
+    rel = a.relu()
 
+    print(f"Addition:       {add}")
+    print(f"Subtraction:    {sub}")
+    print(f"Multiplication: {mul}")
+    print(f"Division:       {div}")
+    print(f"a**3:           {pow}")
+    print(f"a.relu():       {rel}")
+    print(f"sub.relu():     {sub.relu()}")
+
+testing_operations()
--- minigrad/autograd.py
+++ minigrad/autograd.py
@@ -1,11 +1,11 @@
-from typing import Union
-
+from __future__ import annotations
+from typing import Union, Tuple, Optional
 
 class Scalar():
 
     def __init__(self,
              data: Union[float, int],
-             parents = (), # How can I declare a that parents is Tuple[Scalar] here?
+             parents: Tuple[Optional[Scalar], Optional[Scalar]] = (None, None), 
              ):
 
         self.data = data
@@ -17,23 +17,33 @@ class Scalar():
         return f"Scalar(data={self.data}, grad={self.grad})"
 
     # Addition operation override
-    def __add__(self, rhs):
-        new = Scalar(self.data + rhs.data)
+    def __add__(self, rhs: Scalar):
+        new = Scalar(self.data + rhs.data, parents=(self,rhs))
         return new
 
     # Subtraction operation override
-    def __sub__(self, rhs):
-        new = Scalar(self.data - rhs.data)
+    def __sub__(self, rhs: Scalar):
+        new = Scalar(self.data - rhs.data, parents=(self,rhs))
         return new
 
     # Multiplication operation override
-    def __mul__(self, rhs):
-        new = Scalar(self.data * rhs.data)
+    def __mul__(self, rhs: Scalar):
+        new = Scalar(self.data * rhs.data, parents=(self,rhs))
         return new
 
     # Division operation override
-    def __truediv__(self, rhs):
+    def __truediv__(self, rhs: Scalar):
         assert rhs.data != 0, "Division by 0 error in Scalar operation"
 
-        new = Scalar(self.data / rhs.data)
+        new = Scalar(self.data / rhs.data, parents=(self,rhs))
+        return new
+
+    # Power operation, but only support integer / float powers
+    def __pow__(self, power: Union[int, float]):
+        new = Scalar(self.data**power, parents=(self, None))
+        return new
+
+    # Unary operations
+    def relu(self):
+        new = Scalar(max(self.data, 0), parents=(self, None))
         return new
